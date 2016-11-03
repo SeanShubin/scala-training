5ce96cffa7f6c68e2c1ab7fdcc52e3ff2bc099c1
Sean Shubin <sshubin@cj.com>
2016-11-03 13:05:35 -0700

    Replaced JavaConversions with JavaConverters

--- web/src/main/scala/com/seanshubin/scala/training/web/MoreJavaConversions.scala
+++ web/src/main/scala/com/seanshubin/scala/training/web/MoreJavaConversions.scala
@@ -3,11 +3,11 @@ package com.seanshubin.scala.training.web
 import java.util.Map.{Entry => JavaMapEntry}
 import java.util.{Map => JavaMap}
 
-import scala.collection.JavaConversions
+import scala.collection.JavaConverters._
 
 object MoreJavaConversions {
   def toScalaMap(javaMap: JavaMap[String, Array[String]]): Map[String, Seq[String]] = {
     def toTuple2(javaEntry: JavaMapEntry[String, Array[String]]): (String, Seq[String]) = (javaEntry.getKey, javaEntry.getValue)
-    JavaConversions.asScalaSet(javaMap.entrySet()).map(toTuple2).toMap
+    javaMap.entrySet().asScala.map(toTuple2).toMap
   }
 }
